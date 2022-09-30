2fc0c0924b1cf6f01531a36688bf8ce107efdb26
zykscode <111462249+zykscode@users.noreply.github.com>
2022-09-30 02:18:24 +0100

    Update schema.js

--- src/schema.js
+++ src/schema.js
@@ -36,7 +36,8 @@ const typeDefs = gql`
     "The track's illustration to display in track card or track page detail"
     thumbnail: String
     "The track's approximate length to complete, in minutes"
-    length: Int
+    "The track's approximate length to complete, in seconds"
+length: Int @deprecated(reason: "Use durationInSeconds")
     "The number of modules this track contains"
     modulesCount: Int
     "The track's complete description, can be in markdown format"
@@ -45,6 +46,8 @@ const typeDefs = gql`
     numberOfViews: Int
     "The track's complete array of Modules"
     modules: [Module!]!
+    "The track's full duration, in seconds"
+durationInSeconds: Int
   }
 
   "Author of a complete Track or a Module"
@@ -61,12 +64,15 @@ const typeDefs = gql`
     id: ID!
     "The module's title"
     title: String!
-    "The module's length in minutes"
-    length: Int
+   
+  "The module's length in seconds"
+length: Int @deprecated(reason: "Use durationInSeconds")
     "The module's text-based description, can be in markdown format. In case of a video, it will be the enriched transcript"
     content: String
     "The module's video url, for video-based modules"
     videoUrl: String
+    "The module's video duration, in seconds"
+durationInSeconds: Int
   }
 `;
 
